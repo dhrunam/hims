b166385ba970f14e885c206f6973dab84c9619ed
Dhrubajyoti Borah <dhrubajyotiborah@Dhrubajyotis-MacBook-Pro.local>
2023-01-30 14:48:27 +0530

    change item query

--- back_end/hims/configuration/views/item_view.py
+++ back_end/hims/configuration/views/item_view.py
@@ -12,6 +12,20 @@ class ItemList(generics.ListCreateAPIView):
     serializer_class = serializers.ItemSerializer
     # pagination.PageNumberPagination.page_size = 2
 
+    def get_queryset(self):
+        """
+        This view should return a list of all the purchases item  received
+        for the specified order .
+        """
+
+        # order_number = self.request.data['order_no']
+        search_text = self.request.query_params.get('dept_id')
+
+        if(search_text):
+            return models.Item.objects.filter(department=search_text)
+
+        return models.Item.objects.all()
+
 
 class ItemDetails(generics.RetrieveUpdateDestroyAPIView):
     # authentication_classes = (TokenAuthentication,)
