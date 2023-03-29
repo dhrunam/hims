26986ff24a653fba8a78c1c54fd1c4aab66d69ce
Dhrubajyoti Borah <dhrubajyotiborah@Dhrubajyotis-MacBook-Pro.local>
2023-03-29 15:29:43 +0530

    new veiw added to see batchwise items

--- back_end/hims/operation/views/received_item_view.py
+++ back_end/hims/operation/views/received_item_view.py
@@ -99,7 +99,23 @@ class ReceivedItemBatches(generics.ListAPIView):
     #     return super().list(self, request, *arg, **kwargs)
 
 
+class ReceivedItemPerBatch(generics.ListAPIView):
+    # authentication_classes = (TokenAuthentication,)
+    # permission_classes = (IsAuthenticated,)
+    queryset = op_model.ItemReceived.objects.all()
+    serializer_class = serializers.ItemReceivedBatchesSerializer
+    # pagination.PageNumberPagination.page_size = 2
 
+    def get_queryset(self):
+        print('What is this?')
+        """
+        This view should return a list of all the purchases item  received
+        for the specified order .
+        """
+        batch_no = self.request.query_params.get('batch_no')
+        if batch_no:
+            queryset = op_model.ItemReceived.objects.filter(batch_no=batch_no)
+        return queryset
 
 class ReceivedItemDetails(generics.RetrieveUpdateDestroyAPIView):
     # authentication_classes = (TokenAuthentication,)
