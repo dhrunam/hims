7ab6c0b9d3fa6f2980456d9d93dc3eb0bb564d63
Raja Siddharth Raju <rsrajuofficial@gmail.com>
2023-11-21 16:06:10 +0530

    Checkin

--- hims-frontend/src/environment/environment.prod.ts
+++ hims-frontend/src/environment/environment.prod.ts
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-// export const URL = 'http://154.49.243.185';
->>>>>>> 851da07a72473baa010b0ed36d64ac8b67c28bf5
 export const URL = 'http://localhost:8000';
 export const KEY = 'U2FsdGVkX194Y7uE3oWaKaaOVWKXp50Km/Aa1dLr4lk=';
 export const API = {
